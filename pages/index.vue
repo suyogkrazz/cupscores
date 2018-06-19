a773e14871966e412b1a6e456002290fadeb7b86
vayu-suyog <suyog@vayu.com.au>
2018-06-19 15:55:41 +0545

    initilize repo, add dependencies

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+        <v-btn flat>Link Two</v-btn>
+        <v-btn flat>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <div class="cards">
+      <Card v-for="person in people" :key="person.id" :person="person"></Card>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import Card from "~/components/Card.vue"
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State people
+}
+</script>
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
+    sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
